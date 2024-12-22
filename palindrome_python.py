71be1827b8119d3aacbcd542f208781f1bb93fb1
bas <satish.ashwath@gmail.com>
2024-12-22 23:07:40 +0530

    palindrome_python.py

--- palindrome_python.py
+++ palindrome_python.py
@@ -1,11 +1,1 @@
 # Python program to find if  a string is palindrome 
-def is_palindrome(string):
-    cleaned_string = ''.join(char.lower() for char in string if char.isalnum())
-    return cleaned_string == cleaned_string[::-1]
-
-string = input("Enter a string:")
-
-if is_palindrome(string):
-    print(f'"{string}" is a palindrome.')
-else:
-    print(f'"{string}" is not a palindrome.')
